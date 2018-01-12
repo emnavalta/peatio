488fc567aa523b2deafc5f2c42c1072146e52945
emnavalta <emnavalta@gmail.com>
2018-01-12 21:14:34 +0800

    Changed Repository
    scatterp -> emnavalta

--- install1.sh
+++ install1.sh
@@ -61,7 +61,7 @@ echo "export RAILS_ENV=production" >> ~/.bashrc
 source ~/.bashrc
 mkdir -p ~/peatio
 #git clone https://github.com/ctubio/Krypto-trading-bot.git ~/K
-git clone https://github.com/scatterp/peatio.git ~/peatio/current
+git clone https://github.com/emnavalta/peatio.git ~/peatio/current
 #git checkout testing01
 cd ~/peatio/current/
 pwd
